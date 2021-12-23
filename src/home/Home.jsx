cfc2f1ad833677905ac84452a105da3e8f5ed12a
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2021-12-22 22:18:08 -0600

    Initial commit
    first commit

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,24 +1,33 @@
 import React from "react";
 import "./home.css";
-import { useState } from "react";
 
 export default function Home() {
-  const [showText, setShowText] = useState(false);
-  const onClick = () => setShowText(true);
+
   return (
-    <div className="backroundOfHomepage">
-      <button onClick={onClick} type="button" className="introLine">
-        {showText ? <Text /> : null}
-        GENIUS
-      </button>
+    <div className="backroundColor">
+
+      <div className="mainCloudClump">
+          <div className="cloud"></div>
+          <div className="cloud3"></div>
+          <div className="cloud2"></div>
+          <div className="cloud4"></div>
+          <div className="cloud5"></div>
+          <div className="cloud6"></div>
+          <div className="cloud7"></div>
+
+
+
+
+
+      </div>
+     <img className="dog" src="https://i.kym-cdn.com/entries/icons/original/000/018/012/this_is_fine.jpeg" alt="" />
+
+    
+
+     <div className="mainFooter">
+      THIS WILL BE FLAMES 
+     </div>
     </div>
   );
 }
 
-const Text = () => (
-  <div class="container">
-    <div class="box"></div>
-    <div class="box overlay"></div>
-    Hello
-  </div>
-);
--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,9 +1,8 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 export default function NavBar() {
   return (
     <nav>
-      <NavLink to="/">Navbar LINK </NavLink>
+      <div>HELLOO</div>
       <p> this section is for the navbar later</p>
     </nav>
   );
