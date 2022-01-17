90e918c80c97b25d2d6df4a1527dc923693c5322
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2022-01-16 23:06:55 -0600

    Update NavBar.jsx

--- src/navbar/NavBar.jsx
+++ src/navbar/NavBar.jsx
@@ -1,15 +1,32 @@
 import React from "react";
+import { FaYoutube, FaFacebookF,FaHandsHelping,FaRegAddressCard,FaDonate,FaUser } from 'react-icons/fa';
 
 export default function NavBar() {
   return (
+    <div>
+<div className="backroundColor">
+      <div className="home-buttons">
+        <nav>
+        <a className="mainButton__text" href="/volunteer"> <FaHandsHelping className="iconSpace"/>ABOUT ME </a>
+        <a className="mainButton__text" href="/member"> <FaRegAddressCard className="iconSpace"/>  PROJECTS </a>
+        <a className="mainButton__text" href="/donate"> <FaDonate className="iconSpace"/>  EXPERIENCE </a>
+        <a className="mainButton__text" href="/account"> <FaUser className="iconSpace"/>  CONTACT ME </a>
+        <a target="_blank" href="https://www.youtube.com/channel/UCAQTSkwPU9Nnx-606ptbA8A" ><button style={{background: "#3b5998"}} class="social-button" >
+        <FaFacebookF size={30} /></button></a>
+        <a target="_blank" href="https://www.youtube.com/channel/UCAQTSkwPU9Nnx-606ptbA8A" ><button style={{background: "#FF0000"}} class="social-button" >
+        <FaYoutube size={30} /></button></a>
+        </nav>
+      </div>
+      <div className='welcomeSection'>
+
+            </div> 
+    </div>
     <div class="navigation">
   <input type="checkbox" class="navigation__checkbox" id="navi-toggle"/>
   <label for="navi-toggle" class="navigation__button">
                 <span class="navigation__icon">&nbsp;</span>
   </label>
-  <div class="navigation__background">&nbsp;
-  Hello
-  </div>
+  <div class="navigation__background">&nbsp;  </div>
   <nav class="navigation__nav">
     <ul class="navigation__list">
       <li class="navigation__item"><a href="/" class="navigation__link">HOME</a> </li>
@@ -20,5 +37,7 @@ export default function NavBar() {
     </ul>
   </nav>
 </div>
+</div>
+
   );
 }
