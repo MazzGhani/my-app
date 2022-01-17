54cbe2dbbfdbbdf903abf5997c05168c78897b2a
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2022-01-16 23:08:23 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import React from "react";
-import Home from "./pages/home/Home";
 import NavBar from "./navbar/NavBar";
-import About from "./pages/about/About"
 import './navbar/navbar.scss';
 import './pages/home/home.scss';
 import './pages/about/about.scss';
-
-
-
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 function App()  {
