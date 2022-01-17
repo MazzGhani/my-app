b76bf08c982e34f02a0091c9bc814062a4b8a608
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2022-01-16 23:12:53 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from "react";
 import NavBar from "./navbar/NavBar";
 import './navbar/navbar.scss';
-import './pages/home/home.scss';
-import './pages/about/about.scss';
 
 
 function App()  {
