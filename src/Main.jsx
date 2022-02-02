603a8a15c1f0c9f7a1a0e1eb472cd0ebe2a0627b
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2022-02-02 13:12:33 -0600

    updated
    commit

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./main.css"
 export default function Main() {
     return (
-        <div className='mainBackround'>
+        <div >
             <div className='mainTitleName'>
                 <h1>
                     MAZZ GHANI
@@ -10,11 +10,68 @@ export default function Main() {
                     <div className='mainTitleDesc'>
                         JUNIOR DEVELOPPER
                     </div>
+                    <div className='titleRight'>
+                        <marquee behavior="scroll" direction="right">
+                           Hi! Assalamualaikum! Bonjour!
+                        </marquee>
+                    </div>
                 </h1>
             </div>
-            <div className='aboutMe'>
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. In minus quis perspiciatis officia! Corporis, iure recusandae? Consequatur nobis deleniti voluptatum, cum adipisci accusantium, autem sit ut, veniam totam dolores maiores.
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem ducimus nemo accusantium provident, non laboriosam tempore labore inventore, illum reiciendis optio voluptates rerum, dolor voluptate assumenda molestiae quia obcaecati soluta?
+            <div className='mainTitleName'>
+                <h1>EDUCATION</h1>
+                Almost completed my Bachelors in Computer Science at the University of Saskatchewan
+                <br />
+                What did I learn?
+                <br />
+                Computer Science: Python, Java, JavaScript, HTML, CSS, REACT, NodeJS
+                <br />
+                Other: Psychology, Math, Philosophy, Ethics 
+                <br />
+                What did I gain an interest in?
+                <br />
+                Web Development with REACT and NodeJS & Psychology
+                <p>Extras? &#128064;</p>
+                I'm also pursuing a minor in Psychology. WOMBO COMBO with my Computer Science degree.
+                <br />
+                &
+                <br />
+                CompTI A+ Certification
+            </div>
+            <div className='mainskills'>
+            <h1>SKILLS & PROJECTS</h1>
+            <div>
+                MERN STACK PROJECT
+            </div>
+            <div>
+                UNITY APP PROJECT
+            </div>
+            <div>
+                OTHERS
+            </div>
+            </div>
+            <div className='links'>
+                <h1>LINKS</h1>
+                <li>
+                    <a href="https://github.com/MazzGhani">GitHub </a>
+                    <a href="https://www.linkedin.com/in/mazz-ghani-412a37205/">LinkedIn</a>
+                </li>
+            </div>
+            <div>
+                <h1>EXPERIENCE</h1>
+                <div>
+                    IT AND CODING EXPERIENCE +  LABOUR RELATED EXPERIENCE = MAZZ
+                    <div>
+                        IT SUPPORT SPECIALIST 
+                    </div>
+                </div>
+            </div>
+            <div>
+                <h1>ABOUT ME</h1>
+                <div>
+                    INTERESTS
+                    HOBBIES 
+
+                </div>
             </div>
         </div>
     )
