8d266ee0b352a915db1ee654bd477af4dc291b94
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2022-02-23 15:48:59 -0600

    Changed Look

--- src/Main.jsx
+++ src/Main.jsx
@@ -7,50 +7,70 @@ export default function Main() {
                 <h1>
                     MAZZ GHANI
                     <br />
-                    <div className='mainTitleDesc'>
+                    <div className='subTitle'>
                         JUNIOR DEVELOPPER
                     </div>
-                    <div className='titleRight'>
-                        <marquee behavior="scroll" direction="right">
-                           Hi! Assalamualaikum! Bonjour!
-                        </marquee>
+                    <div className='helloQuickLinks'>
+                           <button className='button'>
+                               <a className="buttonStyle" href="#HiSection"> Hi</a></button>
+                            <button className='button'>
+                                <a  className="buttonStyle" href="#salamSection"> Assalamualaikum</a>
+                            </button>
+                            <button className='button'>
+                            <a className="buttonStyle" href="#bonjourSection">Bonjour</a>
+                            </button>
                     </div>
                 </h1>
             </div>
-            <div className='mainTitleName'>
-                <h1>EDUCATION</h1>
-                Almost completed my Bachelors in Computer Science at the University of Saskatchewan
-                <br />
-                What did I learn?
-                <br />
-                Computer Science: Python, Java, JavaScript, HTML, CSS, REACT, NodeJS
-                <br />
-                Other: Psychology, Math, Philosophy, Ethics 
-                <br />
-                What did I gain an interest in?
-                <br />
-                Web Development with REACT and NodeJS & Psychology
-                <p>Extras? &#128064;</p>
-                I'm also pursuing a minor in Psychology. WOMBO COMBO with my Computer Science degree.
-                <br />
-                &
-                <br />
-                CompTI A+ Certification
+            
+            
+
+            <div className='description' >
+                {/* <h1 className=''>EDUCATION</h1> */}
+                <div className='wrapperDescription'>
+                <p >
+                Studying at the <a style={{textDecoration : 'none', color:'green'}} href="https://www.usask.ca/ "> University of Saskatchewan </a> 
+                  and pursuing a Bacherlors in Computer Science (4th year).
+                  <p> I'm also close to finishing my Minor is Psychology in retation to my Bachelors Degree.
+                      I've learned a numerous amount of skills throughout my University career. Ranging from the basics of multiple languages, the complexity of algorithms , and plenty of soft skills with group work.
+                  </p>
+ 
+                </p>
+
+                </div>
+        
             </div>
+
             <div className='mainskills'>
-            <h1>SKILLS & PROJECTS</h1>
-            <div>
-                MERN STACK PROJECT
+            <h1 className='subTitles' id='HiSection'>SKILLS & PROJECTS</h1>
+            <div id='container'>
+         <div className='project1'>
+                <div className='project1Title'>
+                    MERN STACK
+                </div>
             </div>
-            <div>
-                UNITY APP PROJECT
+            <div className='project1'>
+                <div className='project1Title'>
+                    UNITY APP PROJECT
+                </div>
             </div>
-            <div>
-                OTHERS
+            <div className='project1'>
+                <div className='project1Title'>
+                    SKILLS
+                    <li> HTML | CSS</li>
+                    <li>JS | REACT</li>
+                    <li>C# | UNITY </li>
+                    <li> JAVA | C</li>
+                    <li>SCALA | MIPS</li>
+                    <li>PYTHON | BLENDER</li>
+                </div>
             </div>
+
+            </div>
+   <br />
             </div>
             <div className='links'>
-                <h1>LINKS</h1>
+                <h1 className='subTitles'>LINKS</h1>
                 <li>
                     <a href="https://github.com/MazzGhani">GitHub </a>
                     <a href="https://www.linkedin.com/in/mazz-ghani-412a37205/">LinkedIn</a>
@@ -66,11 +86,13 @@ export default function Main() {
                 </div>
             </div>
             <div>
-                <h1>ABOUT ME</h1>
+                <h1 className="subTitles" id='salamSection'>ABOUT ME</h1>
                 <div>
                     INTERESTS
                     HOBBIES 
-
+                </div>
+                <div className="subTitles" id='bonjourSection'>
+                    Languages
                 </div>
             </div>
         </div>
