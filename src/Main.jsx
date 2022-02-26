268644a43e835de49ae679ab2d5db7d46aa36f8e
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2022-02-25 22:33:01 -0600

    Added more personal Information
    testing for qr code

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./main.css"
 import GitHubIcon from "@mui/icons-material/GitHub";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 export default function Main() {
     return (
@@ -31,7 +32,7 @@ export default function Main() {
                 {/* <h1 className=''>EDUCATION</h1> */}
                 <div className='wrapperDescription'>
                 <p >
-                Studying at the <a style={{textDecoration : 'none', color:'green'}} href="https://www.usask.ca/ "> University of Saskatchewan </a> 
+                Studying at the <a target={"_blank"} rel="noreferrer" style={{textDecoration : 'none', color:'green'}} href="https://www.usask.ca/ "> University of Saskatchewan </a> 
                   and pursuing a Bacherlors in Computer Science (4th year).
                   <p> I'm also close to finishing my Minor is Psychology in retation to my Bachelors Degree.
                       I've learned a numerous amount of skills throughout my University career.
@@ -76,17 +77,49 @@ export default function Main() {
             </div>
             <div className='links'>
                 <h1 className='subTitles'>LINKS</h1>
-                <GitHubIcon href="https://github.com/MazzGhani"/>
-                    <a className='linksBottom' href="https://www.linkedin.com/in/mazz-ghani-412a37205/">LinkedIn</a>
+                <a target={"_blank"} rel="noreferrer" href="https://github.com/MazzGhani">
+                    <GitHubIcon style={{fontSize:90}} className='linksBottom'/>
+                </a>
+                <a target={"_blank"} rel="noreferrer" href="https://www.linkedin.com/in/mazz-ghani-412a37205/">
+                    <LinkedInIcon style={{fontSize:90}} className='linksBottom'/>
+                </a>
+
             </div>
-            <div>
-                <h1>EXPERIENCE</h1>
-                <div>
-                    IT AND CODING EXPERIENCE +  LABOUR RELATED EXPERIENCE = MAZZ
-                    <div>
-                        IT SUPPORT SPECIALIST 
-                    </div>
+            <div className='description' >
+                <h1 style={{textAlign:"center"}}  className='subTitles'>EXPERIENCE</h1>
+                <div className='wrapperDescription'>
+                <p> Will be working at <a target={"_blank"} rel="noreferrer" style={{textDecoration : 'none', color:'green' }} href="https://sites.google.com/shopify.com/jrsptadvisor/home"> Shopify </a> as a Junior Support Advisor.
+                <p>
+                    From March 2022 - August 2022 (Contract)
+                </p>
+                <p>
+                    I'm super excited to be working for an established tech company and being able to apply my skills that I've learned so far. The job includes helping customers with their shopify issues via email, text chat, and phone calls.
+                </p>
+                </p>
+                <p >
+                It Support Specialist at <a target={"_blank"} rel="noreferrer" style={{textDecoration : 'none', color:'green'}} href="https://www.facebook.com/Allaboutcellandsahdes/"> All About Cell and Shades </a>. 
+                  <p>
+                      From: 2017 - 2020
+                  </p>
+                  <p> I've learned a majority of my customer service and technical skills from working here.
+                       <br />
+                       Such As:
+                       <li >
+                           Using my BASH and technical skills to help organize the company computer.
+                       </li>
+                       <li>
+                           Helping customers purchase | fix |  debugg their technical issues.
+                       </li>
+                       <li>
+                           Kept tabs on the newest trends and waves of technology.
+                       </li>
+                       <li>Worked with mobiles, computers, and laptops.</li>
+                  </p>
+ 
+                </p>
+
                 </div>
+        
             </div>
             <div>
                 <h1 className="subTitles" id='salamSection'>ABOUT ME</h1>
