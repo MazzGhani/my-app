e6550ea04c320737921508070e9d8ccbfc8017c5
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2022-05-08 14:12:42 -0600

    Updates

---
+++ precache-manifest.8fe180f656273be0a3a4a8a30761bda2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9f57f4603b01028d8c6",
+    "url": "/my-app/static/css/main.5f8ee07f.chunk.css"
+  },
+  {
+    "revision": "b9f57f4603b01028d8c6",
+    "url": "/my-app/static/js/main.5d4ac452.chunk.js"
+  },
+  {
+    "revision": "9930044c31793394defe",
+    "url": "/my-app/static/js/runtime~main.6019dd84.js"
+  },
+  {
+    "revision": "fb7ca313444196fcc5a0",
+    "url": "/my-app/static/js/2.728a9cec.chunk.js"
+  },
+  {
+    "revision": "32ac69fd22d02ebd2d5edb2bea13f0a3",
+    "url": "/my-app/static/media/image2Slide.32ac69fd.PNG"
+  },
+  {
+    "revision": "e71996935dedabc81104fc272fe3c49d",
+    "url": "/my-app/static/media/image3Slide.e7199693.PNG"
+  },
+  {
+    "revision": "3693d8dc43f6d1874d08b89e4e3b548b",
+    "url": "/my-app/index.html"
+  }
+];
