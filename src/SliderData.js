f7794733c33a5e386a38e1309f801604f4852267
Mazz Ghani <78571391+MazzGhani@users.noreply.github.com>
2022-05-21 11:22:48 -0600

    updated slider

--- src/SliderData.js
+++ src/SliderData.js
@@ -2,11 +2,12 @@ import { Hidden } from "@mui/material";
 import image2 from "./images/image2Slide.PNG"
 import image3 from "./images/image3Slide.PNG"
 
+
 export const SliderData=
 [
     {
     key:1,
-    image:"https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+    image:"https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
         
     },
     {
@@ -15,6 +16,7 @@ export const SliderData=
 
     },
     {
-        image:image3
+    image:image3
     }
 ]
+
